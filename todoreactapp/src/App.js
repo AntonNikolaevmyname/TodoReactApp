a562e689c5d8f0f51fcdf00ca8180aa92867c1d0
AntonNikolaev <Anton.Nikolaev.Kazan@gmail.com>
2022-12-11 14:09:41 +0400

    Версия 0.0.1

--- todoreactapp/src/App.js
+++ todoreactapp/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import '@atlaskit/css-reset';
+import initialData from './data/initial-data';
+import Column from './components/column';
+import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 
-function App() {
-  return (
-      <p>Text</p>
-  );
+class App extends React.Component {
+  state = initialData
+
+  onDragEnd = result => {
+    
+  }
+
+  render() {
+    return (
+      <DragDropContext
+
+      >
+        {this.state.columnOrder.map((columnId) => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+          
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    )
+  }
 }
 
 export default App;
