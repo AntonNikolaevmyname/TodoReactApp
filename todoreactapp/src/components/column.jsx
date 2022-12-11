715d5bdf67b1e0451d61b3f8758c14498a2756db
AntonNikolaev <Anton.Nikolaev.Kazan@gmail.com>
2022-12-11 14:13:48 +0400

    Добавил DnD для задач в колонне

--- todoreactapp/src/components/column.jsx
+++ todoreactapp/src/components/column.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Task from './task';
 import styled from 'styled-components';
-import {Droppable} from 'react-beautiful-dnd';
+import { Droppable } from 'react-beautiful-dnd';
+import Task from './task';
 
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
-  border-radius: 2px
+  border-radius: 2px;
 `;
 const Title = styled.h3`
   padding: 8px;
@@ -21,14 +21,16 @@ export default class Column extends React.Component {
       <Container>
         <Title>{this.props.column.title}</Title>
         <Droppable droppableId={this.props.column.id}>
-          {(provided) => 
+          {provided => 
             <TaskList
-            innerRef={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {this.props.tasks.map((task, index) => <Task key={task.id} task={task} index={index}/>)}
-            {provided.placeholder}
-          </TaskList>
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {this.props.tasks.map((task, index) => (
+                <Task key={task.id} task={task} index={index} />
+              ))}
+              {provided.placeholder}
+            </TaskList>
           }
         </Droppable>
       </Container>
--- todoreactapp/src/components/task.jsx
+++ todoreactapp/src/components/task.jsx
@@ -7,16 +7,23 @@ const Container = styled.div`
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
+  background-color: green;
 `;
 
 export default class Task extends React.Component {
   render() {
     return (
     <Draggable draggableId={this.props.task.id} index={this.props.index}>
-      {(provided) => (
-        <Container>{this.props.task.content}</Container>
+      {provided => (
+        <Container
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {this.props.task.content}
+        </Container>
       )}
     </Draggable>
-    )
+    );
   }
 }
